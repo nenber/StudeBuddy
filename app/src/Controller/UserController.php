45e681ed96b2c5d280855cfcc743a64d6cdbc868
celine <celine.ly.99@outlook.fr>
2020-12-30 22:39:40 +0100

    edit user controller and add twig

--- app/src/Controller/UserController.php
+++ app/src/Controller/UserController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Form\EditUserType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\BrowserKit\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -40,6 +42,30 @@ class UserController extends AbstractController
     {
         return $this->render('user/register.html.twig', [
             'controller_name' => 'UserController',
+        ]);
+    }
+
+    /**
+     * @Route("/edit-user", name="edit-user")
+     */
+    public function editUser(Request $request)
+    {
+        $user = $this->getUser();
+        $form = $this->createForm(EditUserType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            $this->addFlash('message', 'Compte mis à jour');
+            return $this->redirectToRoute('user');
+        }
+
+        return $this->render('user/edit-user.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
---
+++ app/src/Form/EditUserType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EditUserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email', EmailType::class)
+            ->add('roles')
+            ->add('password', PasswordType::class)
+            ->add('first_name', TextType::class)
+            ->add('last_name', TextType::class)
+            ->add('phone_number', NumberType::class)
+            ->add('school', TextType::class);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
