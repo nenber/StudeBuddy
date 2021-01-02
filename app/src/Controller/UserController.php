dba47fab2e1557523afd3edd9781d6c8e58ef0d8
celine <celine.ly.99@outlook.fr>
2021-01-02 15:14:37 +0100

    edit password

--- app/src/Controller/UserController.php
+++ app/src/Controller/UserController.php
@@ -6,6 +6,7 @@ use App\Form\EditUserType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 
 /**
@@ -47,7 +48,7 @@ class UserController extends AbstractController
     }
 
     /**
-     * @Route("/edit", name="edit")
+     * @Route("/modifier-compte", name="modifier-compte")
      */
     public function editUser(Request $request)
     {
@@ -62,7 +63,7 @@ class UserController extends AbstractController
             $em->flush();
 
             $this->addFlash('message', 'Compte mis à jour');
-            return $this->redirectToRoute('user_user-account');
+            return $this->redirectToRoute('user_mon-compte');
         }
 
         return $this->render('user/edit.html.twig', [
@@ -71,12 +72,36 @@ class UserController extends AbstractController
     }
 
     /**
-     * @Route("/user-account", name="user-account")
+     * @Route("/mon-compte", name="mon-compte")
      */
     public function userAccount()
     {
         return $this->render('user/user-account.html.twig', [
             'controller_name' => 'UserController',
         ]);
+    }
+
+    /**
+     * @Route("/modifier-mot-de-passe", name="modifier-mot-de-passe")
+     */
+    public function editPass(Request $request, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        if($request->isMethod('POST')){
+            $em = $this->getDoctrine()->getManager();
+
+            $user = $this->getUser();
+
+            if($request->request->get('pass') == $request->request->get('pass2')){
+                $user->setPassword($passwordEncoder->encodePassword($user, $request->request->get('pass')));
+                $em->flush();
+                $this->addFlash('message', 'Mot de passe mis à jour !');
+
+                return $this->redirectToRoute('user_mon-compte');
+            }else{
+                $this->addFlash('error', 'Les deux mots de passe ne sont pas identiques');
+            }
+        }
+
+        return $this->render('user/edit-password.html.twig');
     }
 }
