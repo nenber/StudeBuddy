ce99e26826c5ace1d0aefc56c8a60faf896579eb
celine <celine.ly.99@outlook.fr>
2021-01-16 22:31:17 +0100

    change route bug for reset password

--- app/src/Controller/UserController.php
+++ app/src/Controller/UserController.php
@@ -37,55 +37,46 @@ class UserController extends AbstractController
     /**
      * @Route("/forget-password", name="forget-password")
      */
-    public function forgetPassword(Request $request,MailerInterface $mailer)
+    public function forgetPassword(Request $request, MailerInterface $mailer)
     {
 
         // $mailer->send($email);
-        if($request->request->get("email") != null )
-        {
+        if ($request->request->get("email") != null) {
             $result = $this->getDoctrine()
-            ->getRepository(User::class)
-            ->findOneBy(['email' => $request->request->get("email")]);
+                ->getRepository(User::class)
+                ->findOneBy(['email' => $request->request->get("email")]);
             $em = $this->getDoctrine()->getManager();
 
 
-            if($result == null)
-            {
-                return $this->render('forget-password.html.twig', [
-                            'controller_name' => 'UserController',
-                        ]);
-            }
-            else{
+            if ($result == null) {
+                return $this->render('user/forget-password.html.twig', [
+                    'controller_name' => 'UserController',
+                ]);
+            } else {
                 $uni = $result->getEmail();
                 $token = sha1(random_bytes(strlen($uni)));
                 $result->setToken($token);
                 $em->persist($result);
                 $em->flush();
                 dump($result);
-                $url = $this->generateUrl('user_reset_password',["token" => $result->getToken()]);
+                $url = $this->generateUrl('user_reset_password', ["token" => $result->getToken()]);
 
                 // $restPasswordPage = $this->generateUrl('reset_password',array("token"=>$token));
-              
+
                 $email = (new Email())
-                ->from('noreply@studebuddy.com')
-                ->to($request->request->get("email"))
-                //->cc('cc@example.com')
-                //->bcc('bcc@example.com')
-                //->replyTo('fabien@example.com')
-                //->priority(Email::PRIORITY_HIGH)
-                ->subject('Changement de mot de passe')
-                ->text("Veuillez cliquer sur le lien pour reinitialiser votre mot de passe :"."http://0.0.0.0:8082".$url);
+                    ->from('noreply@studebuddy.com')
+                    ->to($request->request->get("email"))
+                    //->cc('cc@example.com')
+                    //->bcc('bcc@example.com')
+                    //->replyTo('fabien@example.com')
+                    //->priority(Email::PRIORITY_HIGH)
+                    ->subject('Changement de mot de passe')
+                    ->text("Veuillez cliquer sur le lien pour reinitialiser votre mot de passe :" . "http://0.0.0.0:8082" . $url);
 
                 $mailer->send($email);
-
             }
-            
-
-        
-
-
         }
-        return $this->render('user/forget-password.html.twig', [
+        return $this->render('user/forgot-password.html.twig', [
             'controller_name' => 'UserController',
         ]);
     }
@@ -93,56 +84,45 @@ class UserController extends AbstractController
     /**
      * @Route("/reset_password/{token}", name="reset_password")
      */
-    public function resetPassword($token) 
+    public function resetPassword($token)
     {
         $em = $this->getDoctrine()->getManager();
         $result = $em->getRepository(User::class)->findOneBy(['token' => strval($token)]);
-        if($result != null)
-        {
+        if ($result != null) {
             return $this->render('user/reset-password.html.twig', [
-                        'controller_name' => 'UserController',
-                        "email" => $result->getEmail(),
-                        'token' => $result->getToken()
-                    ]);
-        }
-        else
-        {
+                'controller_name' => 'UserController',
+                "email" => $result->getEmail(),
+                'token' => $result->getToken()
+            ]);
+        } else {
             return $this->redirectToRoute("default_index");
         }
-
-        
     }
 
     /**
      * @Route("/change_password/{email}", name="change_password")
      */
-    public function changePassword(Request $request,$email,UserPasswordEncoderInterface $passwordEncoder) 
+    public function changePassword(Request $request, $email, UserPasswordEncoderInterface $passwordEncoder)
     {
         $em = $this->getDoctrine()->getManager();
         $token = $request->query->get('token');
         $result = $em->getRepository(User::class)->findOneBy(['email' => $email]);
         if ($result != null) {
             $newMdp  = $request->request->get("inputMdp");
-            if(preg_match_all("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{6,})^", $newMdp) > 0)
-            {
+            if (preg_match_all("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{6,})^", $newMdp) > 0) {
                 $result->setPassword(
-                            $passwordEncoder->encodePassword(
-                                $result,
-                                $newMdp
-                            )
-                        );
-                        $em->persist($result);
-                        $em->flush();
+                    $passwordEncoder->encodePassword(
+                        $result,
+                        $newMdp
+                    )
+                );
+                $em->persist($result);
+                $em->flush();
+            } else {
+                $this->redirectToRoute('user_reset_password', ['token' => $token]);
             }
-            else{
-                $this->redirectToRoute('user_reset_password',['token'=>$token]);
-            }
-     
         }
-         return $this->redirectToRoute("default_index");
-
-
-        
+        return $this->redirectToRoute("default_index");
     }
 
     /**
